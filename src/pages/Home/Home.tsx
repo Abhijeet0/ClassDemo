1237a5fc12f5a151944dd0b9fd432572d016435d
Abhijeet0 <abhijeetgund0@gmail.com>
2021-09-25 16:22:02 +0530

    login design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  gap: 15px;
+`;
+
+const Name = styled(TextField)`
+  .MuiFormLabel-root.Mui-focused {
+    color: #2d819c;
+  }
+`;
+const Password = styled(TextField)``;
+const Login = styled(Button)`
+  &.MuiButton-contained {
+    background-color: #2d819c;
+    color: #ffffff;
+    font-size: 15px;
+  }
+  &.MuiButton-contained:hover {
+    background-color: red;
+  }
+`;
 
 function Home() {
   const history = useHistory();
@@ -10,9 +37,13 @@ function Home() {
   return (
     <>
       <h2>Home</h2>
-      <Button variant="contained" onClick={navigateToAbout}>
-        Goto About
-      </Button>
+      <Wrapper>
+        <Name variant="outlined" label="Username" />
+        <Password variant="outlined" type="password" label="Password" />
+        <Login variant="contained" onClick={navigateToAbout}>
+          Login
+        </Login>
+      </Wrapper>
     </>
   );
 }
